fdb58cc5f66bdac1d4725c42f6679a426dea6008
Brady Pacha <brady.pacha@gmail.com>
2019-06-14 08:00:20 -0400

    added promotions and conferences to journey data file

--- src/data/journey.js
+++ src/data/journey.js
@@ -1,39 +1,118 @@
 import React from 'react'
-import { FaAward, FaPaperPlane } from 'react-icons/fa'
+import { FaAward, FaPaperPlane, FaBriefcase, FaGraduationCap } from 'react-icons/fa'
 
 import Image1 from './../assets/images/image1.jpg'
 import Image2 from './../assets/images/image2.jpg'
 import Image3 from './../assets/images/image3.jpg'
 
+/*
+ * Time-off Awards
+ * Team Awards
+ * DockerCon/SmashingCon/Fluent/PHP[Tek]/PHP[World]
+ * Moving to Maryland
+ * Chaning to wordpress/round table
+ * */
 const data = [
   {
-    date: 'November 15th 2012',
-    headline: 'Milestone 1',
-    description: `ulputate sapien nec sagittis aliquam
-    malesuada. Quis imperdiet massa tincidunt nunc pulvinar sapien et
-    ligula ullamcorper.`,
-    image: Image1,
-    icon: <FaAward />,
-  },
-  {
-    date: 'November 26th 2011',
-    headline: 'Milestone 2',
+    date: 'April 2019',
+    headline: 'Attended Dockercon',
     description: `ulputate sapien nec sagittis aliquam
     malesuada. Quis imperdiet massa tincidunt nunc pulvinar sapien et
     ligula ullamcorper.`,
     image: Image2,
     icon: <FaPaperPlane />,
   },
-
   {
-    date: 'June 6th 2011',
-    headline: 'Milestone 3',
+    date: 'June 2018',
+    headline: 'Promoted',
     description: `ulputate sapien nec sagittis aliquam
     malesuada. Quis imperdiet massa tincidunt nunc pulvinar sapien et
     ligula ullamcorper.`,
-    image: Image3,
+    image: Image2,
+    icon: <FaBriefcase />,
+  },
+  {
+    date: 'December 2017',
+    headline: 'Attended PHP[World]',
+    description: `ulputate sapien nec sagittis aliquam
+    malesuada. Quis imperdiet massa tincidunt nunc pulvinar sapien et
+    ligula ullamcorper.`,
+    image: Image2,
     icon: <FaPaperPlane />,
   },
+  {
+    date: 'June 2017',
+    headline: 'Attended O\'Reilly Fluent',
+    description: `ulputate sapien nec sagittis aliquam
+    malesuada. Quis imperdiet massa tincidunt nunc pulvinar sapien et
+    ligula ullamcorper.`,
+    image: Image2,
+    icon: <FaPaperPlane />,
+  },
+  {
+    date: 'June 2018',
+    headline: 'Diet Promotion',
+    description: `ulputate sapien nec sagittis aliquam
+    malesuada. Quis imperdiet massa tincidunt nunc pulvinar sapien et
+    ligula ullamcorper.`,
+    image: Image2,
+    icon: <FaBriefcase />,
+  },
+  {
+    date: 'May 2017',
+    headline: 'Attended PHP[Tek]',
+    description: `ulputate sapien nec sagittis aliquam
+    malesuada. Quis imperdiet massa tincidunt nunc pulvinar sapien et
+    ligula ullamcorper.`,
+    image: Image2,
+    icon: <FaPaperPlane />,
+  },
+  {
+    date: 'April 2017',
+    headline: 'Attended Smashing Conf San Francisco',
+    description: `ulputate sapien nec sagittis aliquam
+    malesuada. Quis imperdiet massa tincidunt nunc pulvinar sapien et
+    ligula ullamcorper.`,
+    image: Image2,
+    icon: <FaPaperPlane />,
+  },
+  {
+    date: 'June 2016',
+    headline: 'Promoted',
+    description: `ulputate sapien nec sagittis aliquam
+    malesuada. Quis imperdiet massa tincidunt nunc pulvinar sapien et
+    ligula ullamcorper.`,
+    image: Image2,
+    icon: <FaBriefcase />,
+  },
+  {
+    date: 'August 2013',
+    headline: 'Started Working at Department of Defense',
+    description: `ulputate sapien nec sagittis aliquam
+    malesuada. Quis imperdiet massa tincidunt nunc pulvinar sapien et
+    ligula ullamcorper.`,
+    image: Image1,
+    icon: <FaBriefcase />,
+  },
+  {
+    date: 'August 2013',
+    headline: 'Moved to Maryland',
+    description: `ulputate sapien nec sagittis aliquam
+    malesuada. Quis imperdiet massa tincidunt nunc pulvinar sapien et
+    ligula ullamcorper.`,
+    image: Image1,
+    icon: <FaPaperPlane />,
+  },
+  {
+    date: 'June 2013',
+    headline: 'Graduated from Texas A&M',
+    description: `ulputate sapien nec sagittis aliquam
+    malesuada. Quis imperdiet massa tincidunt nunc pulvinar sapien et
+    ligula ullamcorper.`,
+    image: Image2,
+    icon: <FaGraduationCap />,
+  }
+
 ]
 
 export default data
