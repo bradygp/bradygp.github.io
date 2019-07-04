ffcc5399b027208a5e76bd1b30bc79c1e10aad21
Brady Pacha <brady.pacha@gmail.com>
2019-07-04 19:08:17 -0400

    removed email

--- src/data/contact.js
+++ src/data/contact.js
@@ -2,7 +2,6 @@ const data = {
   twitter: 'https://twitter.com/bradypacha',
   github: 'https://github.com/bradygp',
   linkedin: 'https://www.linkedin.com/in/brady-pacha-0b1a59b/',
-  email: 'brady.pacha@gmail.com',
 }
 
 export default data
